c9164c1b1008dabaccb4855e24ef8c3db8918b6c
Thomas CARRILLO <thomas.carrillo84@gmail.com>
2019-01-08 22:05:38 +0100

    rajout du footer

--- elementsPHP/footer.php
+++ elementsPHP/footer.php
@@ -1,5 +1,28 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript" src="js/dropDown.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <!-- <script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
 <script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script> -->
+
+<footer>
+	<div class="footerRow1">
+
+        <div class="block_coordonnee">
+            <p><strong>Contacts : </strong></p>
+            <p>Email: blasquezdylan0@gmail.com</p>
+            <p>Tel: 06.21.52.70.80</p>
+        </div>
+        <div class="bloc_icones">
+					<a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook" style="font-size: 26px;"></i></a>
+					<a href="https://www.linkedin.com/" target="_blank"><i class="fa fa-linkedin" style="font-size: 26px;"></i></a>
+					<a href="https://www.instagram.com" target="_blank"><i class="fa fa-instagram" style=" font-size: 26px;"></i></a>
+					<a href="https://www.twitter.com/" target="_blank"><i class="fa fa-twitter" style=" font-size: 26px;"></i></a>
+        </div>
+    </div>
+    <div class="footerRow2">
+        <div class="copyright">
+            <em>Tehnecens</em> © 2018. Tous droits réservés.
+        </div>
+    </div>
+</footer>
