f8616ae5ea07387554ce86374ae97fb157596c2f
Thomas CARRILLO <thomas.carrillo84@gmail.com>
2019-01-08 22:02:14 +0100

    Ajout du dev mac dans bdd.php

--- elementsPHP/bdd.php
+++ elementsPHP/bdd.php
@@ -3,16 +3,19 @@ session_start();
 /**
  * DEV WINDOWS
  */
-try{
-    $bdd = new PDO('mysql:host=localhost;dbname=thehub;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch (Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
-}
+// try{
+//     $bdd = new PDO('mysql:host=localhost;dbname=thehub;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+// }
+// catch (Exception $e)
+// {
+//     die('Erreur : ' . $e->getMessage());
+// }
 /**
  * PRODUCTION
  */
 //$bdd = new PDO('mysql:host=db764576347.hosting-data.io;dbname=db764576347;charset=utf8', 'dbo764576347', 'Vitrolles13');
 
-
+/**
+ * DEV MAC
+ */
+$bdd = new PDO('mysql:host=localhost;dbname=projet1;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
